14584c2a56ee67e53e91383f9e186e562a8be625
achub28 <acyberhub28@gmail.com>
2024-12-23 03:16:06 +0530

    Create Home.js

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>CSV to JSON Converter</h1>
+      <p>Upload your CSV file to convert it to JSON</p>
+    </div>
+  );
+}
+
+export default Home;
